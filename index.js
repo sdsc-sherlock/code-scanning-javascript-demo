5c84933187111e24d70f6d8560b190e054941665
Eric Odell <eric.odell@ucop.edu>
2021-05-12 08:13:12 -0700

    bad code test

--- index.js
+++ index.js
@@ -262,6 +262,8 @@ exports.extract = function (cwd, opts) {
       if (win32) return next() // skip links on win for now before it can be tested
       xfs.unlink(name, function () {
         var srcpath = path.resolve(cwd, header.linkname)
+        // good code
+        // var srcpath = path.join(cwd, path.join('/', header.linkname))
 
         xfs.link(srcpath, name, function (err) {
           if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
